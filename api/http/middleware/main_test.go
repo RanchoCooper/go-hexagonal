ab441dc660c71baec21831c36686e1d3533228a5
Rancho <rancho.z@binance.com>
2022-12-29 17:51:23 +0800

    middleware unittest example

---
+++ api/http/middleware/main_test.go
@@ -0,0 +1,22 @@
+package middleware
+
+import (
+    "os"
+    "testing"
+
+    "go-hexagonal/config"
+    "go-hexagonal/util/log"
+)
+
+/**
+ * @author Rancho
+ * @date 2022/12/29
+ */
+
+func TestMain(m *testing.M) {
+    config.Init()
+    log.Init()
+
+    exitCode := m.Run()
+    os.Exit(exitCode)
+}
---
+++ api/http/middleware/middleware_test.go
@@ -0,0 +1,39 @@
+package middleware
+
+import (
+    "net/http"
+    "net/http/httptest"
+    "testing"
+
+    "github.com/gin-gonic/gin"
+    "github.com/stretchr/testify/assert"
+)
+
+/**
+ * @author Rancho
+ * @date 2022/12/29
+ */
+
+func TestTrigger(t *testing.T) {
+    // it's an example unit test for middleware
+
+    t.Run("Exp start", func(t *testing.T) {
+        w := httptest.NewRecorder()
+        ctx, engine := gin.CreateTestContext(w)
+
+        engine.GET("/test",
+            Trigger(1),
+            func(ctx *gin.Context) {
+            })
+
+        ctx.Request, _ = http.NewRequest(http.MethodGet, "/test", nil)
+        ctx.Request.AddCookie(&http.Cookie{
+            Name:  "token",
+            Value: "root-token",
+            Path:  "/",
+        })
+        engine.HandleContext(ctx)
+        assert.Nil(t, ctx.Errors)
+    })
+
+}
